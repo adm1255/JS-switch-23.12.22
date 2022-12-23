13f7cc8284eaea41f1215c5fc6e3102c8a1b4b71
adm1255 <96832058+adm1255@users.noreply.github.com>
2022-12-23 21:25:05 +0600

    hello

---
+++ kanat.js
@@ -0,0 +1,13 @@
+var section = "you"
+
+switch(section){
+    case "you" :
+        console.log("Hello are you name?")
+    case"Kanat" :
+        console.log("My name is kanat")
+
+    default :
+        console.log("you better")
+}
+
+
